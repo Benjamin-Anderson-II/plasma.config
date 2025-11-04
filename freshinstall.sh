615dffcfd98ad0570736e2bfdcbc5cb2769ff57a
Benjamin Anderson II <benjaminalandersonii@gmail.com>
2025-11-04 13:27:50 -0800

    readme, vim, gitignore, install script

--- freshinstall.sh
+++ freshinstall.sh
@@ -14,40 +14,44 @@ su -p -c "cat >> /etc/sudoers <<<'$USER ALL=(ALL:ALL) NOPASSWD:ALL'"
 # Create new ssh key
 ssh-keygen -t ed25519 -C "$email"
 echo -e "Enter the following into GitHub now.\n"
-cat ~/.ssh/id_ed25519.pub
+cat $HOME/.ssh/id_ed25519.pub
 echo -e "\n"
 read -r
 
-# clone .vim
+# get my vim shit in here
 git config --global user.email "$email"
 git config --global user.name "$name"
-git clone git@github.com:Benjamin-Anderson-II/.vim.git ~/.vim
 
-rm ~/.bashrc
-ln -s ~/.config/bash/bashrc ~/.bashrc
+rm $HOME/.bashrc
+ln -s $HOME/.config/bash/bashrc $HOME/.bashrc
 
-# install things
+# install package managers
 sudo apt-get install snap
 sudo apt-get install snapd
 sudo apt install flatpak
 sudo apt install plasma-discover-backend-flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
+# Install applications
+sudo apt install vim
 sudo snap install install obsidian --classic
 sudo snap install spotify
 sudo apt install nvme-cli
 sudo apt install git build-essential cmake extra-cmake-modules libkirigami-dev libkf5style-dev libkf6kcmutils-dev libkf6colorscheme-dev libkf5config-dev libkf5configwidgets-dev libkf5coreaddons-dev libkf5guiaddons-dev libkf6i18n-dev libkf6iconthemes-dev kirigami2-dev libkf6package-dev libkf6service-dev libkf6windowsystem-dev kwayland-dev libx11-dev libkdecorations3-dev libkf5configwidgets-dev libkf5i18n-dev libkf5iconthemes-dev libkf5kcmutils-dev libkf5package-dev libkf5service-dev libkf5wayland-dev libkf5windowsystem-dev libplasma-dev libqt5x11extras5-dev qt6-base-dev qt6-declarative-dev qtbase5-dev qtdeclarative5-dev gettext qt6-svg-dev
-mkdir ~/Packages
-git clone https://github.com/paulmcauley/klassy ~/Packages/klassy
-cd ~/Packages/klassy
+mkdir $HOME/Packages
+git clone https://github.com/paulmcauley/klassy $HOME/Packages/klassy
+cd $HOME/Packages/klassy
 git checkout plasma6.3
 ./install
-cd ~/.config
+cd $HOME/.config
+
+# setup vim
+ln $HOME/.config/vim $HOME/.vim
 
 # Setup Obsidian
-mkdir ~/Obsidian
-git clone git@github.com:Benjamin-Anderson-II/Idekyl.git ~/Obsidian/Idekyl
+mkdir $HOME/Obsidian
+git clone git@github.com:Benjamin-Anderson-II/Idekyl.git $HOME/Obsidian/Idekyl
 
-mkdir -p ~/CLASSES/CS_461
-git clone git@github.com:Benjamin-Anderson-II/NVMeCLI_cmdTest.git ~/CLASSES/CS_461/NVMeCLI_cmdTest
+mkdir -p $HOME/CLASSES/CS_461
+git clone git@github.com:Benjamin-Anderson-II/NVMeCLI_cmdTest.git $HOME/CLASSES/CS_461/NVMeCLI_cmdTest
 sudo reboot
---
+++ vim/README.md
@@ -0,0 +1,8 @@
+# Vim Configuration
+
+If you want to use it, just clone it directly into your home directory.
+
+When you next open vim it will error in the terminal. Just press enter and let `PlugInstall` run. After this run the following comamnd and you're set:
+```
+:so $MYVIMRC
+```
