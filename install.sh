b1e4bd5d0f3daf2dcfbd94a731cf58c20831b8d6
Benjamin Anderson II <benjaminalandersonii@gmail.com>
2025-11-04 18:28:24 -0800

    nerd fonts are in here now

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-email="benjaminalandersonii@gmail.com"
-name="Benjamin Anderson II"
+# Directories
+CONFIG="$HOME/.config"
+OBSIDIAN="$HOME/Obsidian"
+PACKAGES="$HOME/Packages"
+SSH="$HOME/.ssh"
+BASH="$CONFIG/bash"
+FONTS="$CONFIG/fonts"
+
+# Git variables
+EMAIL="benjaminalandersonii@gmail.com"
+NAME="Benjamin Anderson II"
 
 if [ $USER = root ]; then
     echo "Exitting: leave root and re-run"
@@ -12,18 +21,18 @@ fi
 su -p -c "cat >> /etc/sudoers <<<'$USER ALL=(ALL:ALL) NOPASSWD:ALL'"
 
 # Create new ssh key
-ssh-keygen -t ed25519 -C "$email"
+ssh-keygen -t ed25519 -C "$EMAIL"
 echo -e "Enter the following into GitHub now.\n"
-cat $HOME/.ssh/id_ed25519.pub
+cat $SSH/id_ed25519.pub
 echo -e "\n"
 read -r
 
-# get my vim shit in here
-git config --global user.email "$email"
-git config --global user.name "$name"
+# Git config stuff
+git config --global user.email "$EMAIL"
+git config --global user.name "$NAME"
 
 rm $HOME/.bashrc
-ln -s $HOME/.config/bash/bashrc $HOME/.bashrc
+ln -s $CONFIG/bash/bashrc $HOME/.bashrc
 
 # install package managers
 sudo apt-get install snap
@@ -38,20 +47,28 @@ sudo snap install install obsidian --classic
 sudo snap install spotify
 sudo apt install nvme-cli
 sudo apt install git build-essential cmake extra-cmake-modules libkirigami-dev libkf5style-dev libkf6kcmutils-dev libkf6colorscheme-dev libkf5config-dev libkf5configwidgets-dev libkf5coreaddons-dev libkf5guiaddons-dev libkf6i18n-dev libkf6iconthemes-dev kirigami2-dev libkf6package-dev libkf6service-dev libkf6windowsystem-dev kwayland-dev libx11-dev libkdecorations3-dev libkf5configwidgets-dev libkf5i18n-dev libkf5iconthemes-dev libkf5kcmutils-dev libkf5package-dev libkf5service-dev libkf5wayland-dev libkf5windowsystem-dev libplasma-dev libqt5x11extras5-dev qt6-base-dev qt6-declarative-dev qtbase5-dev qtdeclarative5-dev gettext qt6-svg-dev
-mkdir $HOME/Packages
-git clone https://github.com/paulmcauley/klassy $HOME/Packages/klassy
-cd $HOME/Packages/klassy
+mkdir $PACKAGES
+git clone https://github.com/paulmcauley/klassy $PACKAGES/klassy
+cd $PACKAGES/klassy
 git checkout plasma6.3
 ./install.sh
-cd $HOME/.config
+cd $CONFIG
 
 # setup vim
-ln $HOME/.config/vim $HOME/.vim
+ln -s $CONFIG/vim $HOME/.vim
 
 # Setup Obsidian
-mkdir $HOME/Obsidian
-git clone git@github.com:Benjamin-Anderson-II/Idekyl.git $HOME/Obsidian/Idekyl
+mkdir $OBSIDIAN
+git clone git@github.com:Benjamin-Anderson-II/Idekyl.git $OBSIDIAN/Idekyl
 
+# NVMe cli tester
 mkdir -p $HOME/CLASSES/CS_461
 git clone git@github.com:Benjamin-Anderson-II/NVMeCLI_cmdTest.git $HOME/CLASSES/CS_461/NVMeCLI_cmdTest
+cd $CONFIG
+
+# link fonts into .local and reload them
+ln -s $FONTS $HOME/.local/share/fonts
+fc-cache -fv
+
+git remote set-url origin git@github.com:Benjamin-Anderson-II/plasma.config.git
 sudo reboot
